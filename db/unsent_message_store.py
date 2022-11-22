da1d7162eef28e0045947f7a6f7433db66d36417
amansharma612 <amansharma6122002@gmail.com>
2022-11-23 00:18:56 +0530

    Added minor changes to unsent_message_store.py

--- db/unsent_message_store.py
+++ db/unsent_message_store.py
@@ -5,14 +5,14 @@ import time
 
 
 def read_unsent_messages(phone):
-    if os.path.exists('/unsent/{phone}'.format(phone)):
+    if os.path.exists('./unsent/{phone}'.format(phone)):
         file_names = os.path.listdir('/unsent/{phone}')
         update_list = []
         for file_name in file_names:
-            with open('unsent/{phone}/{file_name}', "r") as file:
+            with open('./unsent/{phone}/{file_name}', "r") as file:
                 messages = json.load(file)
                 update_list.append(messages)
-
+            os.remove("./unsent/{phone}/{file_name}")
         return update_list
     else:
         return None
@@ -20,13 +20,11 @@ def read_unsent_messages(phone):
             
 
 
-
-
     
 def store_unread_message(phone1, phone2, message):
-    path = "/unsent/{phone2}/{phone1}.json"
-    if os.path.exists('/unsent/{}'.format(phone2)) :
-        os.mkdir("/unsent/{}".format(phone2))
+    path = "./unsent/{phone2}/{phone1}.json"
+    if os.path.exists('./unsent/{}'.format(phone2)) :
+        os.mkdir("./unsent/{}".format(phone2))
 
     if not (os.path.isfile(path) and os.access(path, os.R_OK)):
         with io.open(os.path.join(path),"w") as db_file:
